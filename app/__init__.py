c768603dd862b497798de99931bd2371e0c16cdb
Akshay Kumar <akshay.gkumar37949@gmail.com>
2020-08-15 01:07:06 -0700

    add some graphical changes

--- app/__init__.py
+++ app/__init__.py
@@ -14,21 +14,21 @@ from firebase_admin   import credentials, firestore
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app_key = {
-    "type" : "service_account",
-    "project_id" : os.environ.get("PROJECT_ID"),
-    "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
-    "private_key": os.environ.get("PRIVATE_KEY").replace("\\n", "\n"),
-    "client_email" : os.environ.get("CLIENT_EMAIL"),
-    "client_id" : os.environ.get("CLIENT_ID"),
-    "auth_uri" : os.environ.get("AUTH_URI"),
-    "token_uri" : os.environ.get("TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.environ.get("AUTH_PROVIDER"),
-    "client_x509_cert_url": os.environ.get("CLIENT_URL")
-}
+# app_key = {
+#     "type" : "service_account",
+#     "project_id" : os.environ.get("PROJECT_ID"),
+#     "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
+#     "private_key": os.environ.get("PRIVATE_KEY").replace("\\n", "\n"),
+#     "client_email" : os.environ.get("CLIENT_EMAIL"),
+#     "client_id" : os.environ.get("CLIENT_ID"),
+#     "auth_uri" : os.environ.get("AUTH_URI"),
+#     "token_uri" : os.environ.get("TOKEN_URI"),
+#     "auth_provider_x509_cert_url": os.environ.get("AUTH_PROVIDER"),
+#     "client_x509_cert_url": os.environ.get("CLIENT_URL")
+# }
 
 app.config.from_object('app.configuration.Config')
-cred = credentials.Certificate(app_key) #make path an ENVIRON VAR
+cred = credentials.Certificate("/Users/akshaykumar/Documents/Projects/FlaskLearning/db_example/key.json") #Switch when Deploying
 firebase_admin.initialize_app(cred)
 
 # Import routing, models and Start the App
